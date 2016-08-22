ef77afffce938c9208f43442789deec46a5a9045
sbasu <sbasu@morphodetection.com>
2016-08-21 17:31:58 -0700

    Added some additional continuation and separation tests.

--- test/parser_continuations_1.js
+++ test/parser_continuations_1.js
@@ -32,7 +32,13 @@ describe('parser continuations', function() {
     const assignment_tests = ['c = \'hello\'\n',
                               'c = \'hello\' \n',
                               'c = \'hello\'% comment\n',
-                              'c = \'hello\' % comments\n'];
+                              'c = \'hello\';% comment\n',
+                              'c = \'hello\',% comment\n',
+                              'c = \'hello\';...\n% comment\n',
+                              'c = \'hello\',...\n% comment\n',
+                              'c = \'hello\' % comments\n',
+                              'c = \'hello\'...\n\n'
+                             ];
     for (const testcase of assignment_tests) {
         it(`should parse this as an assignment:\n${testcase}`, () => {
             const p = parser.parse(testcase);
